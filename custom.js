f3ee939fe0fbc57b8e39bbd98a07611670602dc2
Elmin <111564141+ITDevelopergithub@users.noreply.github.com>
2023-01-30 01:19:29 +0400

    fist commit

---
+++ custom.js
@@ -0,0 +1,553 @@
+var questions = [
+  {
+    question: "	5 %-i 2 olan ədədi tap?",
+    variantA: "43",
+    variantB: "40",
+    variantC: "49",
+    variantD: "35",
+    correctvariant: "40",
+    subject: "Riyaziyyat",
+    level: "Easy",
+  },
+  {
+    question: "(x+3)+(x+1)=12 Tənliyi həll edin?",
+    variantA: "3",
+    variantB: "5",
+    variantC: "4",
+    variantD: "35",
+    correctvariant: "4",
+    subject: "Riyaziyyat",
+    level: "Easy",
+  },
+  {
+    question: "Sürəti 7 olan neçə düzgün olmayan kəsr var??",
+    variantA: "7",
+    variantB: "32",
+    variantC: "4",
+    variantD: "6",
+    correctvariant: "7",
+    subject: "Riyaziyyat",
+    level: "Easy",
+  },
+  {
+    question: "4%-i 5, 6 olan ədədi tapın:",
+    variantA: "48",
+    variantB: "400",
+    variantC: "49",
+    variantD: "140",
+    correctvariant: "140",
+    subject: "Riyaziyyat",
+    level: "Easy",
+  },
+  {
+    question: "0, 1(3) sonsuz dövrü onluq kəsrini adi kəsrə çevirin.",
+    variantA: "2/15",
+    variantB: "26/15",
+    variantC: "12/15",
+    variantD: "7/15",
+    correctvariant: "2/15",
+    subject: "Riyaziyyat",
+    level: "Easy",
+  },
+  {
+    question:
+      "İki ədədin cəmi 5, fərqi 3-dür.Bu ədədlərin hasilini tapın?",
+    variantA: "4",
+    variantB: "7",
+    variantC: "9",
+    variantD: "5",
+    correctvariant: "4",
+    subject: "Riyaziyyat",
+    level: "Middle",
+  },
+  {
+    question: "X≠0 olarsa, 2x(x+y)=5y,x(x-y)=y olduqda x-i tapın?",
+    variantA: "3/4",
+    variantB: "5",
+    variantC: "4/9",
+    variantD: "38",
+    correctvariant: "3/4",
+    subject: "Riyaziyyat",
+    level: "Middle",
+  },
+  {
+    question:
+      "x+y=5,x-y=1    olduğuna görə,  x2-y2+3x+3y ifadəsinin qiymətini tapın?",
+    variantA: "70",
+    variantB: "3",
+    variantC: "40",
+    variantD: "20",
+    correctvariant: "20",
+    subject: "Riyaziyyat",
+    level: "Middle",
+  },
+  {
+    question: "2x+y=5,x-3y=-5 tənliklər sistemindən x+y cəmini tapın?",
+    variantA: "3",
+    variantB: "1",
+    variantC: "4",
+    variantD: "2",
+    correctvariant: "3",
+    subject: "Riyaziyyat",
+    level: "Middle",
+  },
+  {
+    question: "	x+3=2(y-4),y-2=4x tənliklər sistemindən x-i tapın.?",
+    variantA: "2/15",
+    variantB: "26/15",
+    variantC: "12/15",
+    variantD: "7/15",
+    correctvariant: "2/15",
+    subject: "Riyaziyyat",
+    level: "Middle",
+  },
+  {
+    question: "	Hesablayın: (9-2√10)(9+2√10)?",
+    variantA: "12√10",
+    variantB: "7",
+    variantC: "90",
+    variantD: "41",
+    correctvariant: "41",
+    subject: "Riyaziyyat",
+    level: "Difficult",
+  },
+  {
+    question: "Hesablayın: (3√5+√15)2-10√27?",
+    variantA: "34",
+    variantB: "50",
+    variantC: "60",
+    variantD: "38",
+    correctvariant: "60",
+    subject: "Riyaziyyat",
+    level: "Difficult",
+  },
+  {
+    question: "√(5+2√6)=?",
+    variantA: "√3+√2",
+    variantB: "3+√3+√2",
+    variantC: "√3+√7",
+    variantD: "√6+√2",
+    correctvariant: "√3+√2",
+    subject: "Riyaziyyat",
+    level: "Difficult",
+  },
+  {
+    question: "A=√6+1 və b=√6 -1 olduğuna görə, a:b+b:a=?",
+    variantA: "3",
+    variantB: "2,8",
+    variantC: "4",
+    variantD: "2",
+    correctvariant: "2,8",
+    subject: "Riyaziyyat",
+    level: "Difficult",
+  },
+  {
+    question: "(x-3)2+(y+7)2=0 isə, xy=?",
+    variantA: "23",
+    variantB: "-23",
+    variantC: "-21",
+    variantD: "15",
+    correctvariant: "-21",
+    subject: "Riyaziyyat",
+    level: "Difficult",
+  },
+  {
+    question: "Azərbaycan dilində neçə sait var??",
+    variantA: "9",
+    variantB: "12",
+    variantC: "10",
+    variantD: "35",
+    correctvariant: "9",
+    subject: "Az/dili",
+    level: "Easy",
+  },
+  {
+    question: "Azərbaycan dilində neçə samit səs var?",
+    variantA: "24",
+    variantB: "8",
+    variantC: "40",
+    variantD: "32",
+    correctvariant: "24",
+    subject: "Az/dili",
+    level: "Easy",
+  },
+  {
+    question: "	Hansı kar samitin cingiltili qarşılığı yoxdur?",
+    variantA: "H",
+    variantB: "F",
+    variantC: "C",
+    variantD: "G",
+    correctvariant: "H",
+    subject: "Az/dili",
+    level: "Easy",
+  },
+  {
+    question: "Əlifbada neçə hərf var?",
+    variantA: "33",
+    variantB: "9",
+    variantC: "24",
+    variantD: "32",
+    correctvariant: "32",
+    subject: "Az/dili",
+    level: "Easy",
+  },
+  {
+    question: "Bitişdirici samitləri göstərin:",
+    variantA: "n,y,s",
+    variantB: "n,s,m",
+    variantC: "t/e/r",
+    variantD: "b/d",
+    correctvariant: "n,y,s",
+    subject: "Az/dili",
+    level: "Easy",
+  },
+  {
+    question: "	Qrammatik cəhətdən fel bildirir:?",
+    variantA: "Hərəkət",
+    variantB: "Xususiyyət",
+    variantC: "Keyfiyyət",
+    variantD: "Əlamət",
+    correctvariant: "Hərəkət",
+    subject: "Az/dili",
+    level: "Middle",
+  },
+  {
+    question: "Felin neçə sadə şəkli var?",
+    variantA: "6",
+    variantB: "5",
+    variantC: "6",
+    variantD: "8",
+    correctvariant: "6",
+    subject: "Az/dili",
+    level: "Middle",
+  },
+  {
+    question: "	Fel haqqında hansı fikir səhvdir?",
+    variantA: "felin dərəcələri var",
+    variantB: "fel hərəkət bildirir",
+    variantC: "fel təsirli və təsirsiz olur",
+    variantD: "felin 3 zamanı var",
+    correctvariant: "felin dərəcələri var",
+    subject: "Az/dili",
+    level: "Middle",
+  },
+  {
+    question: "Felin quruluşca neçə novu var??",
+    variantA: "3",
+    variantB: "2",
+    variantC: "4",
+    variantD: "7",
+    correctvariant: "3",
+    subject: "Az/dili",
+    level: "Middle",
+  },
+  {
+    question: "Fel cümlədə əsasən hansı cümlə üzvü olur?",
+    variantA: "xəbər",
+    variantB: "mübtəda",
+    variantC: "təyin",
+    variantD: "zərflik",
+    correctvariant: "xəbər",
+    subject: "Az/dili",
+    level: "Middle",
+  },
+  {
+    question: "Hansı əlaqə növü bütün tabeli mürəkkəb cümlələrdə olur?",
+    variantA: "Zaman",
+    variantB: "Qarşılaşdırma",
+    variantC: "Heç biri",
+    variantD: "Səbəb- nəticə",
+    correctvariant: "Zaman",
+    subject: "Az/dili",
+    level: "Difficult",
+  },
+  {
+    question: "Mürəkkəb cümlənin hansı növləri var?",
+    variantA: "tabeli, tabesiz",
+    variantB: "düzəltmə",
+    variantC: "əmr",
+    variantD: "sadə",
+    correctvariant: "tabeli, tabesiz",
+    subject: "Az/dili",
+    level: "Difficult",
+  },
+  {
+    question: "Mürəkkəb cümlənin neçə novu var?",
+    variantA: "2",
+    variantB: "3",
+    variantC: "4",
+    variantD: "5",
+    correctvariant: "3",
+    subject: "Az/dili",
+    level: "Difficult",
+  },
+  {
+    question: "	Hansı bağlayıcı merəkkəb cümlənin tərəflərini bağlamır?",
+    variantA: "ancaq",
+    variantB: "	ilə",
+    variantC: "Belə ki",
+    variantD: "yəni",
+    correctvariant: "ilə",
+    subject: "Az/dili",
+    level: "Difficult",
+  },
+  {
+    question: "Mürəkkəb cümlələr neçə növə ayrılır?",
+    variantA: "2",
+    variantB: "3",
+    variantC: "5",
+    variantD: "6",
+    correctvariant: "2",
+    subject: "Az/dili",
+    level: "Difficult",
+  },
+  {
+    question: "... is a non-count noun. It is usually eaten in summer?",
+    variantA: "ice-cream",
+    variantB: "bread",
+    variantC: "soup",
+    variantD: "salad",
+    correctvariant: "ice-cream",
+    subject: "Inglis/dili",
+    level: "Easy",
+  },
+  {
+    question: "... is a non-count noun. It is made of fruit?",
+    variantA: "juice",
+    variantB: "honey",
+    variantC: "water",
+    variantD: "chocolate",
+    correctvariant: "juice",
+    subject: "Inglis/dili",
+    level: "Easy",
+  },
+  {
+    question:
+      "	A .... is a count noun. We use it to travel from one place to another?",
+    variantA: "car",
+    variantB: "travel",
+    variantC: "traffic",
+    variantD: "accident",
+    correctvariant: "car",
+    subject: "Inglis/dili",
+    level: "Easy",
+  },
+  {
+    question:
+      "This door is made of .......... This is a non-count noun.?",
+    variantA: "steel",
+    variantB: "key",
+    variantC: "latch",
+    variantD: "handle",
+    correctvariant: "steel",
+    subject: "Inglis/dili",
+    level: "Easy",
+  },
+  {
+    question: "I would like a ......... of tea. This is a count noun.",
+    variantA: "cup",
+    variantB: "cheese",
+    variantC: "ice",
+    variantD: "sugar",
+    correctvariant: "cup",
+    subject: "Inglis/dili",
+    level: "Easy",
+  },
+  {
+    question: "Do you own the flat or do you ... it?",
+    variantA: "rent",
+    variantB: "landlord",
+    variantC: "buy",
+    variantD: "lend",
+    correctvariant: "rent",
+    subject: "Inglis/dili",
+    level: "Middle",
+  },
+  {
+    question: "	I have got a great ... from my balcony.?",
+    variantA: "view",
+    variantB: "lift",
+    variantC: "path",
+    variantD: "front garden",
+    correctvariant: "view",
+    subject: "Inglis/dili",
+    level: "Middle",
+  },
+  {
+    question: "Jack ... me that he was fed up with his job.?",
+    variantA: "told",
+    variantB: "tell",
+    variantC: "say",
+    variantD: "said",
+    correctvariant: "told",
+    subject: "Inglis/dili",
+    level: "Middle",
+  },
+  {
+    question: "Dont just stand there! ... something!?",
+    variantA: "say",
+    variantB: "said",
+    variantC: "told",
+    variantD: "tell",
+    correctvariant: "say",
+    subject: "Inglis/dili",
+    level: "Middle",
+  },
+  {
+    question:
+      "Dont ... anybody what I ... . Its a secret just between us.?",
+    variantA: "tell*said",
+    variantB: "tell*say",
+    variantC: "told*say",
+    variantD: "told*said",
+    correctvariant: "tell*said",
+    subject: "Inglis/dili",
+    level: "Middle",
+  },
+  {
+    question: "She was determined to be ... author.?",
+    variantA: "-",
+    variantB: "the",
+    variantC: "an",
+    variantD: "a",
+    correctvariant: "an",
+    subject: "Inglis/dili",
+    level: "Difficult",
+  },
+  {
+    question: "The Director was ... the best parking-place.?",
+    variantA: "allocated",
+    variantB: "provided",
+    variantC: "left",
+    variantD: "donated",
+    correctvariant: "allocated",
+    subject: "Inglis/dili",
+    level: "Difficult",
+  },
+  {
+    question: "She recently became ... minister in the new government.?",
+    variantA: "a",
+    variantB: "-",
+    variantC: "the",
+    variantD: "an",
+    correctvariant: "a",
+    subject: "Inglis/dili",
+    level: "Difficult",
+  },
+  {
+    question:
+      "I could hear the sound of ... helicopter in the distance.?",
+    variantA: "a",
+    variantB: "-",
+    variantC: "the",
+    variantD: "an",
+    correctvariant: "a",
+    subject: "Inglis/dili",
+    level: "Difficult",
+  },
+  {
+    question: "My mother s cousin ... me Â£5 , 000 in her will.?",
+    variantA: "left",
+    variantB: "provide",
+    variantC: "donated",
+    variantD: "presented",
+    correctvariant: "left",
+    subject: "Inglis/dili",
+    level: "Difficult",
+  },
+];
+var minute = 4;
+var second = 60;
+var correct = 0;
+
+setInterval(saat, 1000);
+function saat() {
+  second--;
+  san.innerHTML = second;
+  deg.innerHTML = minute;
+  if (second == 0) {
+    second = 60;
+    minute--;
+    if (minute == 0) {
+      minute = 0;
+      over.innerHTML = "Bitdi!";
+      failure.innerHTML =
+        "İmtahanda verilen vaxti sona yetdi,Imtahana yeniden daxil olmag isdeyirsinizse zehmet olmazsa f5 duymesini basasiniz!!!";
+      document.querySelector(".w3-red").style.display = "block";
+    }
+  }
+}
+
+// let n=0;
+// var elementss,elementsA,elementsB,elementsC,elementsD;
+// var newEle,newEleA,newEleB,newEleC,newEleD;
+
+let fenn;
+document.querySelectorAll(".lessons .lesson button").forEach((ele) => {
+  ele.addEventListener("click", function () {
+    document.querySelector(".levels").style.display = "block";
+    ele.parentElement.parentElement.style.display = "none";
+    fenn = questions.filter((sub) => sub.subject == ele.innerText);
+  });
+});
+let seviye;
+document.querySelectorAll(".levels .level button").forEach((newbtn) => {
+  newbtn.addEventListener("click", function () {
+    document.querySelector(".container1").style.display = "block";
+    newbtn.parentElement.parentElement.style.display = "none";
+    seviye = fenn.filter((lvl) => lvl.level == newbtn.innerText);
+    doldur();
+  });
+});
+
+let z=0;
+function doldur() {
+  cavaba.innerHTML = seviye[z].variantA;
+  cavabb.innerHTML = seviye[z].variantB;
+  cavabc.innerHTML = seviye[z].variantC;
+  cavabd.innerHTML = seviye[z].variantD;
+  sual.innerHTML = seviye[z].question;
+  dogrucavab=seviye[z].correctvariant 
+}
+
+right_button.addEventListener("click", next);
+function next() {
+  document.querySelectorAll(".insecond").forEach((x) => {
+    x.style.backgroundColor = "transparent";
+  });
+  z++
+  doldur()
+}
+
+left_button.addEventListener("click", prev );
+function prev(){
+  z--;
+  doldur()
+}
+
+ document.querySelectorAll('.insecond').forEach(duz=>{
+      duz.addEventListener("click",function(){
+      if (duz.innerText == dogrucavab) {
+      duz.style.backgroundColor = "green";
+      setTimeout(function () {
+        next();
+      }, 1000);
+    } else {
+      duz.style.backgroundColor = "red";
+      document.querySelectorAll(".insecond").forEach((y) => {
+        if (y.innerText == dogrucavab) {
+          y.style.backgroundColor = "green";
+          setTimeout(function () {
+            next();
+          }, 1000);
+        }
+      });
+    }
+      })
+})
+
+finis.addEventListener("click", function () {
+  document.querySelector(".main").style.display = "none";
+  document.querySelector(".second1").style.display = "flex";
+});
+
